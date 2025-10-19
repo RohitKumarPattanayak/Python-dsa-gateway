b9fe9b2140f9b7fa6560ae5ee8a4caf8a2607244
Rohit kumar <rohitkumarpattanayak@gmail.com>
2025-10-19 20:17:48 +0530

    ROHIT-added an optimized method to ch-3 -> q2

--- Cp3-ExtractionOfDigits/q2-basic.py
+++ Cp3-ExtractionOfDigits/q2-basic.py
@@ -10,21 +10,6 @@
 # 1) i have user 'augmented statements' here feel free to research
 # - count+=1 is count = count + 1
 # - number_cpy//=10 is number_cpy = number_cpy//10
-
-
-# attempt-2-start :
-number = int(input('enter the number : '))
-number_cpy = number
-count = 0 
-while (number_cpy):
-    count+=1
-    number_cpy//=10
-print(count)
-# attempt-2-end : 
-
-# Note Verdict and Comparison Notes
-# -------------------------------------------------------------
-# Attempt-1 (String-based)
 # Pros:
 # 1) Very simple and short.
 # 2) Easy to read and write, no loops needed.
@@ -33,6 +18,17 @@ print(count)
 # 2) Fails for negative numbers and whitespace.
 # 3) In interviews, it’s often seen as avoiding the algorithmic logic.
 
+# attempt-2-start :
+# number = int(input('enter the number : '))
+# number_cpy = number
+# count = 0 
+# while (number_cpy):
+#     count+=1
+#     number_cpy//=10
+# print(count)
+# attempt-2-end : 
+
+# Note 
 # Attempt-2 (Mathematical approach using //)
 # Pros:
 # 1) True numeric logic — uses integer division and loops.
@@ -43,7 +39,26 @@ print(count)
 # 1) Slightly longer code.
 # 2) Requires careful handling for the zero case.
 
-# Verdict:
-# ✅ Attempt-2 is better for interviews and learning since it shows deeper understanding
-#    of number manipulation and algorithmic logic.
-# ⚡ Attempt-1 is fine for quick scripts but not ideal to demonstrate programming skills.
+
+# attempt-3-start :
+from math import *
+number = int(input('enter the number : '))
+print(int(log10(number)+1))
+# attempt-3-end : 
+
+# Note 
+# Attempt-3 ( trick using log 10 approach )
+# explore what log to the base 10 is - The term log 10 means the common logarithm of the number 10, which asks the question: "To what power must 10 be raised to get the number 10?". The answer is 1, because \(10^{1}=10\). 
+# trick when get a log to bas 10 of any number adding one to it and rounding it of gives you number of digit the umber had
+# example
+# n = 5438, log10(5438) = 3.735 , 3.735 + 1 = 4.735 , int(4.735) = 4 hence the number of digits of n
+# n = 177715, log10(177715) = 5.248 , 5.248 + 1 = 6.248 , int(6.248) = 6 hence the number of digits of n 
+
+# Pros:
+# 1) True numeric logic.
+# 2) Very simple and short.
+# Cons:
+# 1) Not beginner friendly.
+
+
+
